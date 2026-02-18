618c84861c36dfadd14776e532243b189b593c3e
bidwh.etl2010@gmail.com <bidwh.etl2010@gmail.com>
2026-02-18 06:33:40 +0000

    Changes

---
+++ models/example/tgt-customer.sql
@@ -0,0 +1,19 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select * from customers
+
+)
+
+select *
+from source_data
